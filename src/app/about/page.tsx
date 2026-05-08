b689c07c32538ce5e3cb2166fc2134211d5d014a
Jhoan Camilo <acostadevice@gmail.com>
2026-05-08 12:59:52 -0500

    Cambios en About

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+// Desarrollado por Jhoan Camilo // 
+export default function AboutPage() {
+  return (
+    <main>
+      <h1>About</h1>
+      <p>Esta es la página About.</p>
+    </main>
+  );
+}
